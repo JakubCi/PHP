5718f132baa6bed381811109ede9a00199759ce0
JakubCi <90244126+JakubCi@users.noreply.github.com>
2021-09-29 13:57:31 +0200

    Add files via upload

---
+++ zmiennephp.php
@@ -0,0 +1,9 @@
+<?php
+include_once "zmiennehtml.html";
+
+    define('imie','Jakub');
+    define('kierunek','programista');
+    define('wiek','17');
+    echo "mam na imie ".imie."i mam ".wiek." lat" ;
+    echo 'Jestem uczniem '.kierunek;
+?>
